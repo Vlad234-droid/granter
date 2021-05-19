1454a2146bac4629c7377f4b45071cfd89a11ad8
Vlad Baryshpolets <vladikvladvita@gmail.com>
2021-05-19 17:45:20 +0300

    optimise aside block, fix bug on welome page, drop_down__list

---
+++ src/components/LayoutDashboard/Aside/config.js
@@ -0,0 +1,48 @@
+export const activeItems = [
+  {
+    key: 'active',
+    to: '/active-claims/',
+    text: 'Active Claims',
+    className: '',
+  },
+  {
+    key: 'future',
+    to: '/future-claims/',
+    text: 'Future Claims',
+    className: '',
+  },
+  {
+    key: 'completed',
+    to: '/',
+    text: 'Completed Claims',
+    className: '',
+  },
+  {
+    key: 'profile',
+    to: '/profile',
+    text: 'Profile',
+    className: 'profile',
+  },
+];
+export const subMenuItems = [
+  {
+    key: 'library-introduction',
+    to: '/documents/introduction',
+    text: 'Introduction',
+  },
+  {
+    key: 'library-financial',
+    to: '/documents/financial',
+    text: 'Financial',
+  },
+  {
+    key: 'library-technical',
+    to: '/documents/technical',
+    text: 'Technical',
+  },
+  {
+    key: 'library-submission',
+    to: '/documents/submission',
+    text: 'Submission',
+  },
+];
