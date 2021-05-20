4d6f48a2c2323626e2602341fb0af5f0970ddca1
ScooterOk <scooterok@gmail.com>
2021-05-20 10:15:23 +0300

    up

--- src/pages/DocumentsPage/OnDeleteModal/index.js
+++ src/pages/DocumentsPage/OnDeleteModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button } from 'antd';
 import { Link } from 'react-router-dom';
 
@@ -8,7 +8,20 @@ import iconPdf from '../../../assets/img/icon-pdf.svg';
 
 import './style.scss';
 
-const OnDeleteModal = ({ visible, onClose }) => {
+const OnDeleteModal = ({ visible, onClose, deleteList, onDeleteFile }) => {
+  useEffect(() => {
+    console.log('deleteList', deleteList);
+  }, [deleteList]);
+
+  const onDeleteList = () => {
+    deleteList.forEach((item) => {
+      if (!item.has_unresolved_comments) {
+        onDeleteFile(item.claim_id, item.id);
+        onClose();
+      }
+    });
+  };
+
   return (
     <Modal
       className="documents-remove-modal"
@@ -18,16 +31,45 @@ const OnDeleteModal = ({ visible, onClose }) => {
       width={700}
       onCancel={onClose}
       closeIcon={<CloseIconModal />}>
-      <h2>Are you sure you want to delete this Documents?</h2>
-      <div className="documents-remove-modal__text">If you delete - you cannot get it back.</div>
-      <div className="documents-remove-modal__actions">
-        <Button type="button" onClick={onClose}>
-          Cancel
-        </Button>
-        <Button type="primary" danger>
-          Delete
-        </Button>
-      </div>
+      {deleteList.filter((item) => item.has_unresolved_comments).length ? (
+        <>
+          <h2>Unfortunately, you can't delete this file(s)</h2>
+          <div className="documents-remove-modal__text">
+            The following files cannot be deleted because it contains an{' '}
+            <b>unresolved comments. Please, contact your manager to discuss the comments</b>
+          </div>
+          <ul className="documents-remove-modal__list">
+            {deleteList
+              .filter((item) => item.has_unresolved_comments)
+              .map((item) => (
+                <li key={`delete-file-${item.id}`}>
+                  <img src={iconPdf} alt="" />
+                  <span>{item.original_name}</span>
+                </li>
+              ))}
+          </ul>
+          <div className="documents-remove-modal__actions">
+            <Button type="button" onClick={onClose}>
+              Back
+            </Button>
+            <Button type="primary" onClick={onDeleteList} danger>
+              Delete the rest
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Are you sure you want to delete this Documents?</h2>
+          <div className="documents-remove-modal__actions">
+            <Button type="button" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button type="primary" onClick={onDeleteList} danger>
+              Delete
+            </Button>
+          </div>
+        </>
+      )}
     </Modal>
   );
 };
--- src/pages/FutureClaimsPage/index.js
+++ src/pages/FutureClaimsPage/index.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Skeleton } from "antd";
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Skeleton } from 'antd';
 
-import Layout from "../../components/LayoutDashboard/Layout";
-import { getfutureClaimData } from "../../core/services";
+import Layout from '../../components/LayoutDashboard/Layout';
+import { getfutureClaimData } from '../../core/services';
 
-import "./style.scss";
+import './style.scss';
 
 let count = 0;
 
@@ -16,7 +16,7 @@ const FutureClaimsPage = () => {
   useEffect(() => {
     if (user) {
       getfutureClaimData(user.id).then((data) => {
-        console.log("getfutureClaimData", data);
+        console.log('getfutureClaimData', data);
         setFutureClaimData(data);
       });
     }
@@ -24,28 +24,28 @@ const FutureClaimsPage = () => {
   }, [user]);
 
   return (
-    <Layout isLogged={false} className='dashboard'>
-      <div className='future-claims'>
+    <Layout isLogged={false} className="dashboard">
+      <div className="future-claims">
         {!futureClaimData ? (
           <Skeleton active />
         ) : (
-          <ul className='future-claims__list'>
+          <ul className="future-claims__list">
             {futureClaimData.map((item) => (
-              <li key={`claim-${item.id}`} className='future-claim'>
-                <div className='future-claim--title'>{item.title}</div>
-                <div className='future-claim--row'>
+              <li key={`claim-${item.id}`} className="future-claim">
+                <div className="future-claim--title">{item.title}</div>
+                <div className="future-claim--row">
                   <span>R&D Tax Relief Claim</span>
                   <b>01/01/2018</b>
                 </div>
-                <div className='future-claim--row'>
+                <div className="future-claim--row">
                   <span>Estimated completion</span>
                   <b>01/01/2018</b>
                 </div>
-                <div className='future-claim--row'>
+                <div className="future-claim--row">
                   <span>Estimated benefit</span>
                   <b>£ 20 000</b>
                 </div>
-                <div className='future-claim--row'>
+                <div className="future-claim--row">
                   <span>Consultant fee</span>
                   <b>20%</b>
                 </div>
