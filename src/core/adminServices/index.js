54fc5116fa568456f91b3050afbdd84a52ca30f5
ScooterOk <scooterok@gmail.com>
2021-06-09 11:33:38 +0300

    up

--- src/core/adminServices/index.js
+++ src/core/adminServices/index.js
@@ -2,3 +2,4 @@ export * from './clientServices.js';
 export * from './claimServices.js';
 export * from './documentsServices.js';
 export * from './projectServices.js';
+export * from './settingsServices.js';
